d2b057d41e5588e745cee47fb9dff4db3724ba74
Tiago Garcia <thiaggoggarcia@gmail.com>
2024-11-18 17:48:59 -0300

    Criando a view para as rotas

--- src/main/routes/person_routes.py
+++ src/main/routes/person_routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.view.http_types.http_request import HttpRequest
 
 person_routes_bp = Blueprint("person_routes", __name__)
 
 @person_routes_bp.route("/person/create", methods=["POST"])
 def create_person():
+    http_request = HttpRequest(body=request.json)
+    # Mandar http_request para views
     return jsonify({"Hello":"World"}), 200
+
---
+++ src/view/person_register_view.py
@@ -0,0 +1,20 @@
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+
+class PersonRegisterView:
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        body = http_request.body
+        person_name = body.get("person_name")
+        person_age = body.get("person_age")
+        self.__validate_inputs(person_name,person_age)
+        
+        #Chamar a logica response
+        return HttpResponse(body={"Registrando":"Pessoa "}, status_code=201)
+        
+    def __validate_inputs(self, person_name: any, person_age: any) -> None:
+        if (
+            not person_name or
+            not person_age or
+            not isinstance(person_name, str) or
+            not isinstance(person_age, int)
+        ): raise Exception("Entrada inválida ao tentar cadastrar uma pessoa")
